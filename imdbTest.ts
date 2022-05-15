5741aa68fc1560388ffd5ae7b18d85fb4a3f8b47
Your Name <you@example.com>
2022-05-15 19:52:57 +0200

    Hola

--- imdbTest.ts
+++ imdbTest.ts
@@ -2,3 +2,8 @@ import{ Professional}from "./imdb"
 
 let antonio = new Professional("Antonio", 62, "Cine", 70, 170, "Negro", "Marrones", "Actor", false, "Española", 0, "Actor");
 antonio.print();
+
+let mario = new Professional("mario", 62, "Cine", 70, 170, "Negro", "Marrones", "Actor", false, "Española", 0, "Actor");
+mario.print();
+
+
