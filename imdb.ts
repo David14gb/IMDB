8f03629a58854eac4c05fd54ea3494b98265436e
David14gb <davidgarciab14@gmail.com>
2022-05-15 19:44:33 +0200

    Subida para Versión 1

--- imdb.ts
+++ imdb.ts
@@ -30,7 +30,7 @@ export class Professional{
         print():void{
             console.log("Name: " + this.name + "\n" + "Age: " + this.age + "\n" + "Genre: " + this.genre + "\n" + "Weight: " + this.weight
             + "\n" + "Height: " + this.height + "\n" + "HairColor: " + this.hairColor + "\n" + "EyeColor" + this.eyeColor + "\n" + "Race: " + this.race
-            + "\n" + "IsRetired: " + this.isRetired + "\n" + "Nationality: " + this.nationality + "\n" + "OscarsNUmber: " + this.oscarsNumber
+            + "\n" + "IsRetired: " + this.isRetired + "\n" + "Nationality: " + this.nationality + "\n" + "OscarsNumber: " + this.oscarsNumber
             + "\n" + "Porfession: " + this.profession);
         }
 
